b49b2afadbaf6c3494fbeaa83781c2d0cc1bd0d8
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-12-26 17:54:28 +0300

    added the car image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,9 @@ const Hero = () => {
         />
       </div>
       <div className="hero__image-container">
-        <div className="hero__image"></div>
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
       </div>
     </div>
   );
