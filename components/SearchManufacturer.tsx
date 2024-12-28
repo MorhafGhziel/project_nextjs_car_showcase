c76c6e60093e84956bb27532af06a49aaac23831
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-12-28 21:38:04 +0300

    added combobox button

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,12 +1,31 @@
+"use client";
+
 import { SearchManufacturerProps } from "@/types";
 import React from "react";
 import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              alt="Car Logo"
+              width={20}
+              height={20}
+              className="ml-4"
+            />
+          </Combobox.Button>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
