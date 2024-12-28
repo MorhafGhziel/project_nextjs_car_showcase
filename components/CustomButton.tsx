04125ee1a10c27f5a7b8318a7d6925cfe0ade559
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-12-28 20:51:29 +0300

    added comments to the custombutton code

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
-import { CustomButtonProps } from "@/types";
+import { CustomButtonProps } from "@/types"; // Importing the type for the props
 
 const CustomButton = ({
   title,
@@ -11,13 +10,12 @@ const CustomButton = ({
   btnType,
 }: CustomButtonProps) => {
   return (
+    // Button element with dynamic styles and click handler
     <button
       disabled={false}
       type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
-      onClick={() => {
-        handleClick;
-      }}
+      onClick={handleClick}
     >
       <span className={`flex-1`}>{title}</span>
     </button>
