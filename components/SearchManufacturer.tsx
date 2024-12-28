72549143e942d3a2a27bbf0a5f967b1a23430dc0
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-12-28 21:52:33 +0300

    added searchbar

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { SearchManufacturerProps } from "@/types";
-import React from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
+import { useState, Fragment } from "react";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -22,6 +23,21 @@ const SearchManufacturer = ({
               className="ml-4"
             />
           </Combobox.Button>
+
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterEnter={() => setQuery("")}
+          ></Transition>
         </div>
       </Combobox>
     </div>
