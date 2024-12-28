fd62b5a4d6e55a98ae8d35139dad085d5c386cf9
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-12-28 18:06:42 +0300

    added btntype as a prop to the custombutton

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
         <Link href="/" className="flex justify-center items-center">
           <Image
-            src="/Logo.svg"
+            src="./Logo.svg"
             alt="Car Hub Logo"
             width={118}
             height={18}
