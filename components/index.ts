fe7fedc01d26af748a0c355f7b5b5768ee525808
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-12-28 21:14:47 +0300

    created SearchManufacturer component

--- components/index.ts
+++ components/index.ts
@@ -4,5 +4,14 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
