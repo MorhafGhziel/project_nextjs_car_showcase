52bb75477c46902048ec83e5e336ae87e8c87664
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-12-28 18:14:00 +0300

    added border to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100"></footer>
+  );
 };
 
 export default Footer;
