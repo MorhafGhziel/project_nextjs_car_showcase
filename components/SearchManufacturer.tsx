7a0826808e1c7a03adde22b867b3d5923a7a47f8
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-12-29 21:36:21 +0300

    added comments

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -6,17 +6,20 @@ import Image from "next/image";
 import { useState, Fragment } from "react";
 
 const SearchManufacturer = ({
-  manufacturer,
-  setManufacturer,
+  manufacturer, // Current selected manufacturer
+  setManufacturer, // Function to update the selected manufacturer
 }: SearchManufacturerProps) => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(""); // State to handle the search query
+
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      {/* Combobox for selecting a manufacturer */}
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
+          {/* Combobox button with an image */}
           <Combobox.Button className="absolute top-[14px]">
             <Image
-              src="/car-logo.svg"
+              src="/car-logo.svg" // Icon for the dropdown
               alt="Car Logo"
               width={20}
               height={20}
@@ -24,20 +27,24 @@ const SearchManufacturer = ({
             />
           </Combobox.Button>
 
+          {/* Input field for filtering manufacturers */}
           <Combobox.Input
-            className="search-manufacturer__input"
-            placeholder="Volkswagen"
-            displayValue={(manufacturer: string) => manufacturer}
-            onChange={(e) => setQuery(e.target.value)}
+            className="search-manufacturer__input" // Custom input styles
+            placeholder="Volkswagen" // Default placeholder text
+            displayValue={(manufacturer: string) => manufacturer} // Displays the selected value
+            onChange={(e) => setQuery(e.target.value)} // Updates the query state on input change
           />
 
+          {/* Transition for dropdown animation */}
           <Transition
-            as={Fragment}
+            as={Fragment} // Wraps multiple children without adding extra DOM nodes
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
             afterEnter={() => setQuery("")}
-          ></Transition>
+          >
+            {/* Dropdown menu could go here */}
+          </Transition>
         </div>
       </Combobox>
     </div>
