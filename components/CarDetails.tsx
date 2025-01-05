b1106fbd489d6239fdb94d845735e2d08bb4596c
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-06 01:45:39 +0300

    added two more small car images

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -74,6 +74,24 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                           className="object-contain"
                         />
                       </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src="/hero.png"
+                          alt="Car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src="/hero.png"
+                          alt="Car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
                     </div>
                   </div>
                 </Dialog.Panel>
