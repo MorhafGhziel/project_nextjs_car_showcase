c6a97fe384eeb57327ff9e20752ba4778cec709d
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-06 00:40:26 +0300

    created the props

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,4 +1,11 @@
+import { CarProps } from "@/types";
 import React from "react";
+
+interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  Car: CarProps;
+}
 
 const CarDetails = () => {
   return <div>CarDetails</div>;
