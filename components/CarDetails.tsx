ab746ab5689efd44687d95aae7b69895ca2cd2f7
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-08 17:40:20 +0300

    mapped over the key and the value

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -98,6 +98,17 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <h2 className="font-semibold text-xl capitalize">
                       {car.make} {car.model}
                     </h2>
+                    <div className="flex flex-wrap mt-3 gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div
+                          className="flex justify-between gap-5 w-full text-right"
+                          key={key}
+                        >
+                          <h4> {key} </h4>
+                          <p> {value} </p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
