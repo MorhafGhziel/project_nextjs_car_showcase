0efcd0ed79b5c2f3896d539bc59c177a8ac6baa4
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-12 08:33:39 +0300

    done the input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -40,8 +40,17 @@ const SearchBar = () => {
           width={25}
           className="absolute w-[20px] h-[20px] ml-4"
         />
-        <input />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
