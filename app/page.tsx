e4c39e46bf6c29fa60d24bcd0eddf3548ed78b64
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-02-21 21:39:05 +0300

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home({ searchParams }: HomeProps) {
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold"> </h1>
-          <p> our </p>
+          <p> vadvsvs </p>
         </div>
 
         <div className="home__filters">
