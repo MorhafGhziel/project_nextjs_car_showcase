e12a13d14de0007a45573fb84e1ca80dae966c39
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-03-06 05:58:36 +0300

    new edits

--- constants/index.ts
+++ constants/index.ts
@@ -49,7 +49,7 @@ export const yearsOfProduction = [
   { title: "2017", value: "2017" },
   { title: "2018", value: "2018" },
   { title: "2019", value: "2019" },
-  { title: "2020", value: "2020" },
+  { title: "20220", value: "2020" },
   { title: "2021", value: "2021" },
   { title: "2022", value: "2022" },
   { title: "2023", value: "2023" },
